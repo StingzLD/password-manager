c60695a12125874d565ea3e1b826cb7847c88a39
Corey Hastings <stingzld@gmail.com>
2023-01-08 23:19:09 -0600

    Added password generator and password add to list

--- main.py
+++ main.py
@@ -1,14 +1,54 @@
 from tkinter import *
+import random
 
 
 # ---------------------------- PASSWORD GENERATOR -------------------------- #
+LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+           'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
+           'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+           'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+PASSWORD_LENGTH = 20
+
+num_letters = random.randint(8, 10)
+num_numbers = random.randint(4, 6)
+num_symbols = PASSWORD_LENGTH - num_letters - num_numbers
+
+
 def generate_password():
-    print("Password Generated")
+    password_list = []
+    for _ in range(num_letters):
+        password_list.append((random.choice(LETTERS)))
+    for _ in range(num_numbers):
+        password_list.append((random.choice(NUMBERS)))
+    for _ in range(num_symbols):
+        password_list.append((random.choice(SYMBOLS)))
+
+    # Shuffle list and join into a string
+    random.shuffle(password_list)
+    password = ''.join(password_list)
+
+    # Display password in password entry field
+    passwd_entry.delete(0, END)
+    passwd_entry.insert(END, password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_password():
-    print("Password Added")
+    # Collect data from entry fields
+    website = web_entry.get()
+    username = uname_entry.get()
+    password = passwd_entry.get()
+
+    # Append password file
+    with open("passwords.txt", "a") as file:
+        file.write(f"{website}  |  {username}  |  {password}\n")
+
+    # Clear the website and password entry fields
+    web_entry.delete(0, END)
+    web_entry.focus()
+    passwd_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------------ #
